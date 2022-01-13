793ffd6db5fd3c9a842153bae71ef2bfe64f940d
Suyash <suyashsingh1313@gmail.com>
2022-01-13 17:46:25 +0530

    home header added

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+// hamburger menu
+let ham = document.querySelector('.hamburger');
+let menu = document.querySelector('.menu');
+let close_ham = document.querySelector('.close-ham');
+
+ham.addEventListener('click',show);
+close_ham.addEventListener('click',hide);
+
+function show(){
+    menu.classList.add('active');
+}
+
+function hide(){
+    menu.classList.remove('active');
+}
